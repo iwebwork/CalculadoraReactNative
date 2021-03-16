f5846f49c0be5e21ced91d8155070a433dee239f
Igor <iwebwork.adm@gmail.com>
2021-03-16 00:05:52 -0300

    Styles gerais

---
+++ src/assets/style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    title: {
+        alignItems:'center',
+        backgroundColor:'#FFFFFF'
+    },
+    
+    body:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems:'center'
+    }
+})
+
